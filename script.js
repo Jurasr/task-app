42e3e0f754123aa6dff967aad8aed8ab26b63b46
Juras <jrabacauskas@gmail.com>
2025-06-20 13:34:24 +0300

    added some js

--- script.js
+++ script.js
@@ -1,19 +1,60 @@
 const addTaskInput = document.querySelector('.add-task__input');
 const addTaskButton = document.querySelector('.add-task__button');
 const statusMessage = document.querySelector('.add-task__status-message');
+const taskList = document.querySelector('.task-list');
+
+const addTaskEvents = (task) => {
+    task.addEventListener('click', (event) => {
+        const targetClassName = event.target.className;
+        if (targetClassName === 'task__edit') {
+            console.log('edit');
+        } else if (targetClassName === 'task__delete') {
+            console.log('delete');
+        }
+    });
+}
+
+const createTask = (taskTitle) => {
+    const task = document.createElement('li');
+    task.classList.add('task')
+    task.innerHTML = `
+        <input type="text" class="task__title" value="${taskTitle}" disabled>
+        <div>
+            <span class="task__edit">Edit</span>
+            <span class="task__delete">Delete</span>
+        </div>
+    `;
+    addTaskEvents(task);
+    return task;
+}
+
+const updateStatusMessage = (textContent, color) => {
+    statusMessage.textContent = textContent;
+    statusMessage.style.color = color;
+    statusMessage.style.display = 'block';
+    statusMessage.style.opacity = '1';
+}
+
+const disappearStatusMessage = () => {
+    setTimeout(() => {
+        statusMessage.style.opacity = 0;
+    }, 1000);
+    setTimeout(() => {
+        statusMessage.style.display = 'none';
+    }, 2000)
+}
 
 addTaskButton.addEventListener('click', () => {
-    if (addTaskInput.value.length > 3) {
-        statusMessage.textContent = "Task created successfully";
-        statusMessage.style.display = 'block';
-        statusMessage.style.opacity = '1';
-        statusMessage.style.color = "#28ac2e";
-        setTimeout(() => {
-            statusMessage.style.display = 'none';
-        }, 1000);
+
+    const inputText = addTaskInput.value;
+
+    if (inputText.length > 3) {
+        taskList.appendChild(createTask(inputText));
+        updateStatusMessage('Task created successfully', '#28ac2e');
+        disappearStatusMessage();
+        addTaskInput.value = '';
     } else {
-        statusMessage.textContent = "Task should be longer than 3 characters";
-        statusMessage.style.color = "red";
-        statusMessage.style.display = 'block';
+        updateStatusMessage('Task should be longer than 3 characters', 'red')
+        disappearStatusMessage();
     }
 })
